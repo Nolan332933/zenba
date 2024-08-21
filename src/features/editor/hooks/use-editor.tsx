da5c97852b7d3e96ca790d791d89cc52465f41ab
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-08-22 02:01:52 +0700

    fabric js set-up

--- src/features/editor/hooks/use-editor.tsx
+++ src/features/editor/hooks/use-editor.tsx
@@ -10,6 +10,15 @@ export const useEditor = () => {
       initialCanvas: fabric.Canvas;
       initialContainer: HTMLDivElement;
     }) => {
+      fabric.Object.prototype.set({
+        cornerColor: "#FFF",
+        cornerStyle: "circle",
+        borderColor: "3b82f6",
+        borderScaleFactor: 1.5,
+        transparentCorners: false,
+        borderOpacityWhenMoving: 1,
+        cornerStrokeColor: "#3b82f6",
+      });
       const initialWorkspace = new fabric.Rect({
         width: 900,
         height: 1200,
